234d472aa9a45ed92176dc495f70e062f653cc15
Taku10 <tvmadziwa5@gmail.com>
2023-01-14 13:49:15 +0200

    init

--- pages/cart.js
+++ pages/cart.js
@@ -5,6 +5,18 @@ const Cart = () => {
   return (
     <div className='cart-container'>
         <CartBanner/>
+        <div className='cart-products-container'>
+          <table className='cart-table'>
+           <tbody className='cart-table-body'>
+            <tr className='cart-headers'>
+              <th>PRODUCT</th>
+              <th>PRICE</th>
+              <th>QUANTITY</th>
+              <th>TOTAL</th>
+            </tr>
+           </tbody>
+          </table>
+        </div>
     </div>
   )
 }
