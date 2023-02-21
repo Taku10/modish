492b8a0fc7829f7e2added61148a30273547b158
Taku10 <tvmadziwa5@gmail.com>
2023-02-21 17:46:14 +0200

    init

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CartBanner } from '../components'
+import {HiMinus, HiPlus} from 'react-icons/hi'
 
 const Cart = () => {
   return (
@@ -9,10 +10,25 @@ const Cart = () => {
           <table className='cart-table'>
            <tbody className='cart-table-body'>
             <tr className='cart-headers'>
+              <th></th>
               <th>PRODUCT</th>
               <th>PRICE</th>
               <th>QUANTITY</th>
               <th>TOTAL</th>
+            </tr>
+            <tr className='cart-items'>
+              <td className='cart-image'>
+                <img src="" alt="image-1" />
+              </td>
+              <td>Men's Watch</td>
+              <td>R244</td>
+              <td>
+                <div className='item-counter'>
+                  <button><HiMinus /></button>
+                  <p>0</p>
+                  <button><HiPlus/></button>
+                </div>
+              </td>
             </tr>
            </tbody>
           </table>
