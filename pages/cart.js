7068c75d892e701d5119475a8e5ee4b9308025f2
Taku10 <tvmadziwa5@gmail.com>
2023-02-22 15:11:33 +0200

    init

--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { CartBanner } from '../components'
-import {HiMinus, HiPlus} from 'react-icons/hi'
+import { HiMinus, HiPlus } from 'react-icons/hi'
+import bag from '../images/bag.png'
+import watch from '../images/watch.png'
+
 
 const Cart = () => {
   return (
     <div className='cart-container'>
-        <CartBanner/>
-        <div className='cart-products-container'>
-          <table className='cart-table'>
-           <tbody className='cart-table-body'>
+      <CartBanner />
+      <div className='cart-products-container'>
+        <table className='cart-table'>
+          <tbody className='cart-table-body'>
             <tr className='cart-headers'>
               <th></th>
               <th>PRODUCT</th>
@@ -16,23 +19,48 @@ const Cart = () => {
               <th>QUANTITY</th>
               <th>TOTAL</th>
             </tr>
-            <tr className='cart-items'>
-              <td className='cart-image'>
-                <img src="" alt="image-1" />
-              </td>
-              <td>Men's Watch</td>
-              <td>R244</td>
-              <td>
-                <div className='item-counter'>
-                  <button><HiMinus /></button>
-                  <p>0</p>
-                  <button><HiPlus/></button>
-                </div>
-              </td>
-            </tr>
-           </tbody>
-          </table>
-        </div>
+            <div className="cart-items-container">
+              <tr className='cart-items'>
+                <td className='cart-image-container'>
+                  <div className='cart-image'>
+                    <img src={bag.src} alt="image-1" />
+                  </div>
+                </td>
+                <td>Men's Watch</td>
+                <td>R244</td>
+                <td>
+                  <div className='item-counter'>
+                    <button><HiMinus className='item-minus' /></button>
+                    <p className='item-qty'>0</p>
+                    <button><HiPlus className='item-plus' /></button>
+                  </div>
+                </td>
+                <td>R 2000</td>
+
+              </tr>
+              <tr className='cart-items'>
+                <td className='cart-image-container'>
+                  <div className="cart-image">
+                    <img src={watch.src} alt="image-1" />
+                  </div>
+                </td>
+                <td>Men's Watch</td>
+                <td>R244</td>
+                <td>
+                  <div className='item-counter'>
+                    <button><HiMinus className='item-minus' /></button>
+                    <p className='item-qty'>0</p>
+                    <button><HiPlus className='item-plus' /></button>
+                  </div>
+                </td>
+                <td>R 2000</td>
+
+              </tr>
+            </div>
+
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
