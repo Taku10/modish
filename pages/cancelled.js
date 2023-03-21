f9d59068da3787d63c4741f72c9e32f84ecafb2a
Taku10 <tvmadziwa5@gmail.com>
2023-03-21 18:11:43 +0200

    stripe config

---
+++ pages/cancelled.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cancelled = () => {
+  return (
+    <div className='cancelled-container'>
+      <h1>Cancelled</h1>
+    </div>
+  )
+}
+
+export default Cancelled
---
+++ pages/success.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div className='success-container'>
+      <h1>Success</h1>
+    </div>
+  )
+}
+
+export default Success
