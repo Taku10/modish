4eb39a305076e18e19e867eae842ffa332e23048
Taku10 <tvmadziwa5@gmail.com>
2023-03-26 17:29:05 +0200

    filter by category and search filter fixed

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,16 +1,28 @@
 import React, {useState} from 'react'
 
 const Categories = ({data}) => {
-    const[filter, setFilter]=useState(data);
-    console.log(filter);
+    const[sorted, setSorted]=useState(data);
+    
+    console.log("Categories:", sorted)
+
+    const handleFilter = (e)=>{
+        const updated = e.target.value;
+        
+        if(updated === "all"){
+          setSorted(data);
+        }else if(updated === "women"){
+          const filtered = data.filter((item)=> item.category === "women")
+          setSorted(filtered)
+        }
+       }
     
     return (
         <div className='categories-container'>
             <h2>Categories</h2>
             <div className='categories-options'>
-                <p>All</p>
-                <p>Women</p>
-                <p>Men</p>
+                <button value = 'all' onClick={handleFilter}>All</button>
+                <button value ='women' onClick={handleFilter}>Women</button>
+                <button  value ='men' onClick={handleFilter}>Men</button>
                 <p>Accesories</p>
             </div>
         </div>
--- components/ShopProducts.jsx
+++ components/ShopProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useRef } from 'react'
 import Categories from './Categories'
 import NotFound from './NotFound'
 import Search from './Search'
@@ -7,19 +7,52 @@ import Product from './Product'
 import InfiniteScroll from 'react-infinite-scroller';
 import { AiOutlineSearch } from 'react-icons/ai'
 import { Context } from '../context/StateContext'
+import { AiFillCloseCircle } from 'react-icons/ai'
 
 
 const ShopProducts = ({ products }) => {
   const [data, setData] = useState(products);
   const useStateContext = useContext(Context);
-  const{search, setSearch} = useStateContext;
-
+  const { search, setSearch } = useStateContext;
   const search_data = data.filter((item) => item.title.toLowerCase().includes(search)) //search input
+  const [sorted, setSorted] = useState(products)
 
- 
+  const ref = useRef(null)
 
 
 
+
+
+  const handleFilter = (e) => {
+    const updated = e.target.value;
+
+    if (updated === "all") {
+      setSorted(data);
+      ref.current.value = "";
+      setSearch("")
+    } else if (updated === "women") {
+      const filtered = data.filter((item) => item.category === "women")
+      setSorted(filtered)
+      ref.current.value = "";
+      setSearch("")
+    } else if (updated === 'men') {
+      const filtered = data.filter((item) => item.category === "men")
+      setSorted(filtered)
+      ref.current.value = "";
+      setSearch("")
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSorted(search_data)
+  }
+
+  const clearSearch = () => {
+    ref.current.value = "";
+    setSearch("")
+    console.log(search)
+  }
 
 
 
@@ -27,22 +60,30 @@ const ShopProducts = ({ products }) => {
     <div className='shop-products-container'>
       <div className='shop-products-wrapper'>
         <div className='shop-grid-left'>
-          <Categories data = {data} />
-         
+          <div className='categories-container'>
+            <h2>Categories</h2>
+            <div className='categories-options'>
+              <button value='all' onClick={handleFilter}>All</button>
+              <button value='women' onClick={handleFilter}>Women</button>
+              <button value='men' onClick={handleFilter}>Men</button>
+            </div>
+          </div>
         </div>
         <div className='shop-grid-right'>
-        <div className='search-container'>
-            <form action="">
-              <input type="text" placeholder='Search Products...' onChange={(e) => setSearch(e.target.value)} />
-              <AiOutlineSearch className='search-icon' />
+          <div className='search-container'>
+            <form onSubmit={handleSubmit}>
+              <input type="text" ref={ref} placeholder='Search Products...' onChange={(e) => setSearch(e.target.value)} />
+              {search.length >= 1 ? <AiFillCloseCircle title='Clear Search' className='clear-search' onClick={clearSearch} /> : <AiOutlineSearch className='search-icon' title='Search' />}
             </form>
           </div>
           <div className='all-products'>
             <div className='search-data'>
-              {search_data.length < 1 ? <NotFound />:
-                search_data.map((item, i) => (
+              {/*Display NotFound component if searched product does not exist */}
+               {search_data.length < 1 ? <NotFound /> : 
+                search ? search_data.map((item, i) => (
                   <Product item={item} key={i} />
-                ))}
+                )) : sorted.map((item, i) => (
+                  <Product item={item} key={i} />))}
             </div>
 
           </div>
